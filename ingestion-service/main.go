e6892507a5893244ac2f0a4b0c380257f022aa8b
Rishav Bairagya <rishavbairagya@gmail.com>
2025-11-25 19:12:22 +0000

    MVP Setup & High-Throughput Ingestion Service (Go)

---
+++ ingestion-service/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	//"fmt"
+	"log"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/gofiber/fiber/v2"
+
+)
+
+type LogEntry struct {
+	Service   string    `json:"service"`
+	Level     string    `json:"level"`
+	Message   string    `json:"message"`
+}
+
+var ctx =  context.Background()
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	_, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		log.Fatal("Cannot connect to Redis", err)
+	}
+	app := fiber.New()
+
+	app.Post("/ingest", func(c *fiber.Ctx) error {
+		var entry LogEntry
+		if err := c.BodyParser(&entry); err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Cannot parse JSON"})
+		}
+
+		err := rdb.XAdd(ctx, &redis.XAddArgs{
+			Stream: "logs",
+			Values: map[string]interface{}{
+			"service":   entry.Service,
+			"level":     entry.Level,
+			"message":   entry.Message,
+			"timestamp": time.Now().Unix(),
+			},
+		}).Err()
+		if err != nil {
+			return c.Status(500).JSON(fiber.Map{"error": "Failed to store log entry"})
+		}
+		return c.JSON(fiber.Map{"status":"queued"})
+	})
+
+	log.Fatal(app.Listen(":3080"))
+}
